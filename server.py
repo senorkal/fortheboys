0be2d20bff76cca3159ad1bfc1e12ec0fa70a00a
quo <44597465+senorkal@users.noreply.github.com>
2024-06-27 05:19:00 +0000

    Mundo guardado 2024-06-27

---
+++ server.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+# Custom version
+# mohist / catserver   Install forge   first
+# purpur               Install fabric  first
+# snapshot             Install vanilla first
+
+# Ngrok region
+# Code           Place
+#-----------     ---------------------------
+# ap	          Asia/Pacific (Singapore)
+# au		  Australia (Sydney)
+# eu		  Europe (Frankfurt)
+# in		  India (Mumbai)
+# jp		  Japan (Tokyo)
+# sa		  South America (São Paulo)
+# us		  United States (Ohio)
+# us-cal-1	  United States (California)
+
+import requests,os
+os.system(f"python3 ./sel2_0_3.pyc")
